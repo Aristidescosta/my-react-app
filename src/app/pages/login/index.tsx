2c4fe37c07e8680e8436909921c545d7b890e4bf
Aristidescosta <aristidiscosta200@gmail.com>
2022-09-16 16:26:32 +0100

    refactor: UseRef

--- src/app/pages/login/index.tsx
+++ src/app/pages/login/index.tsx
@@ -1,16 +1,20 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleEnter = () => {
+  const handleEnter = useCallback(() => {
     console.log(email, password);
-  };
+    if (inputPasswordRef.current) {
+      inputPasswordRef.current.focus();
+    }
+  }, [email, password]);
 
-  //O useMemo só recuperará o valor memoizado quando o array receber uma atualização. Esta otimização ajuda a evitar cálculos caros em cada renderização.
-  const emailLenght = useMemo(() => {
-    console.log("executou");
+  //O useMemo só recuperará o valor memorizado quando o array receber uma atualização. Esta otimização ajuda a evitar cálculos caros em cada renderização.
+  const emailLength = useMemo(() => {
     return email.length * 1000;
   }, [email.length]);
   useEffect(() => {
@@ -23,11 +27,14 @@ export const Login = () => {
 
   return (
     <form>
-      <p>Quantidade de letras no email: {emailLenght}</p>
+      <p>Quantidade de letras no email: {emailLength}</p>
       <label>
         <span>Email</span>
         <input
           type="text"
+          onKeyDown={(e) =>
+            e.key === "Enter" ? inputPasswordRef.current?.focus() : undefined
+          }
           onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
@@ -39,6 +46,7 @@ export const Login = () => {
           type="password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
+          ref={inputPasswordRef}
         />
       </label>
 

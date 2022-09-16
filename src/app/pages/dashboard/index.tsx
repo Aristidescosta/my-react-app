d92f32440f531a266b4389dbee08964d298b8529
Aristidescosta <aristidiscosta200@gmail.com>
2022-09-16 16:34:45 +0100

    refactor: useRef com objetos simples

--- src/app/pages/dashboard/index.tsx
+++ src/app/pages/dashboard/index.tsx
@@ -1,10 +1,16 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
+  const counterRef = useRef({ counter: 0 });
+
   return (
     <>
       <p>Dashboard</p>
       <Link to="/entrar">Entrar</Link>
+      <p>Contador: {counterRef.current.counter}</p>
+      <button onClick={() => counterRef.current.counter++} type="button">Somar</button>
+      <button onClick={() => console.log(counterRef.current.counter)} type="button">Somar Console</button>
     </>
   );
 };

9b2a08bca06109e6531f52d4089dc1a8acbb4254
Aristidescosta <aristidiscosta200@gmail.com>
2022-09-16 15:52:46 +0100

    refactor: useMemo

--- src/app/pages/login/index.tsx
+++ src/app/pages/login/index.tsx
@@ -1,15 +1,50 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useMemo, useState } from "react";
 
 export const Login = () => {
-	const navigate = useNavigate()
-	const handleClick = () =>{
-		navigate("/pagina-inicial")
-	}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEnter = () => {
+    console.log(email, password);
+  };
+
+  //O useMemo só recuperará o valor memoizado quando o array receber uma atualização. Esta otimização ajuda a evitar cálculos caros em cada renderização.
+  const emailLenght = useMemo(() => {
+    console.log("executou");
+    return email.length * 1000;
+  }, [email.length]);
+  useEffect(() => {
+    if (window.confirm("Você é homem?")) {
+      console.log("homem");
+    } else {
+      console.log("Mulher");
+    }
+  }, []);
 
   return (
-    <>
-      <div>Login</div>
-      <button onClick={handleClick}>Voltar a página inicial</button>
-    </>
+    <form>
+      <p>Quantidade de letras no email: {emailLenght}</p>
+      <label>
+        <span>Email</span>
+        <input
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>Senha</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <button type="button" onClick={handleEnter}>
+        Entrar
+      </button>
+    </form>
   );
 };

f54f6e30547d42cac6627f096e209c9ce9258d2a
Aristidescosta <aristidiscosta200@gmail.com>
2022-09-17 16:19:07 +0100

    refactor: Melhorando a lista

--- src/app/pages/dashboard/index.tsx
+++ src/app/pages/dashboard/index.tsx
@@ -1,32 +1,62 @@
 import React, { useCallback, useState } from "react";
 
-export const Dashboard = () => {
-  const [list, setList] = useState<string[]>([]);
-  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) =>{
-    if(e.key === "Enter"){
-      if(e.currentTarget.value.trim().length === 0) {
-        return
-      }
-      else{
-        const value = e.currentTarget.value
-        e.currentTarget.value = "";
-        setList((oldList) => {
-          return [...oldList, value]
-        })
-      }
-    }
-  }, [])
+interface IList {
+  title: string;
+  isSelect: boolean;
+}
 
+export const Dashboard = () => {
+  const [list, setList] = useState<IList[]>([]);
+  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> =
+    useCallback((e) => {
+      if (e.key === "Enter") {
+        if (e.currentTarget.value.trim().length === 0) return;
+
+        const value = {
+          title: e.currentTarget.value,
+          isSelect: false,
+        };
+
+        e.currentTarget.value = "";
+
+        setList((oldList) => {
+          if (oldList.some((listItem) => listItem.title === value.title))
+            return oldList;
+          return [...oldList, value];
+        });
+      }
+    }, []);
 
   return (
     <>
       <p>Lista</p>
-      <input type="text" 
-        onKeyDown={handleInputKeyDown}
-      />
+      <input type="text" onKeyDown={handleInputKeyDown} />
+      <p>{list.filter((listItem) => listItem.isSelect).length}</p>
       <ul>
-        {list.map((value, index) => {
-          return <li key={index}>{value}</li>;
+        {list.map((listItem, index) => {
+          return (
+            <li key={index}>
+              <input
+                type="checkbox"
+                checked={listItem.isSelect}
+                onChange={() => {
+                  setList((oldList) => {
+                    return oldList.map((oldListItem) => {
+                      const newIsSelect =
+                        oldListItem.title === listItem.title
+                          ? !oldListItem.isSelect
+                          : oldListItem.isSelect;
+                      return {
+                        ...oldListItem,
+                        isSelect: newIsSelect,
+                      };
+                    });
+                  });
+                }}
+              />
+              {listItem.title}
+            </li>
+          );
         })}
       </ul>
     </>
